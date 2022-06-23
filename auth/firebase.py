aee34f3663247b835a397c78017bf5f63f18fa19
Rahul kumar Agrahari <66835286+BrooCode@users.noreply.github.com>
2022-06-23 15:31:55 +0530

    Update firebase.py

--- auth/firebase.py
+++ auth/firebase.py
@@ -2,14 +2,14 @@ import pyrebase
 
 
 firebaseConfig = {
-  'apiKey': "AIzaSyDEOKvIMA_jCkhrsOqhrDeTprMWcGT0L0E",
-  'authDomain': "infyulabs-4adc0.firebaseapp.com",
-  'databaseURL': "https://infyulabs-4adc0-default-rtdb.firebaseio.com",
-  'projectId': "infyulabs-4adc0",
-  'storageBucket': "infyulabs-4adc0.appspot.com",
-  'messagingSenderId': "1009466381176",
-  'appId': "1:1009466381176:web:e3889591c7ddd9c15c2e8d",
-  'measurementId': "G-BYT7Y1YTEK"
+  'apiKey': "*",
+  'authDomain': "*",
+  'databaseURL': "*",
+  'projectId': "*",
+  'storageBucket': "*",
+  'messagingSenderId': "*",
+  'appId': "*",
+  'measurementId': "*"
 };  
 
 
